b509498e0a398764755509ed82789a5f72032d57
Галина Слесарева <galaslesareva@gmail.com>
2025-02-09 00:36:47 +0300

    goto css to scss

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,4 +1,4 @@
-import './employees-list-item.css';
+import './employees-list-item.scss';
 
 const EmployeesListItem = (props) => {
     const { name, salary, increase, rise, onDelete, onToggleProp, onChangeProp } = props
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/app";
 
-import "./index.css";
+import "./index.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
